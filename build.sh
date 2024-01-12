d9405ad8a144b61da1c18e88ead1723343beb607
greenmashimaro@gmail.com <greenmashimaro@gmail.com>
2024-01-12 17:15:11 +0800

    feat: add variable

--- build.sh
+++ build.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# 获取 $mode 参数
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --mode)
+      # 如果 --mode 后有参数，则将其赋值给 mode 变量
+      if [ "$#" -gt 1 ]; then
+        mode="$2"
+        echo "The value of the Mode variable is: $mode"
+      else
+        echo "Error: Missing parameter value after -- mode."
+        exit 1
+      fi
+      shift 2
+      ;;
+    *)
+      # 其他未知参数，你可以根据需要添加处理逻辑
+      echo "Unknown parameter: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$mode" = "production" ]; then
+  source variables.sh
+else
+  # Can be changed to other environment variables
+  source variables.sh
+fi
+
 source variables.sh
 
 imageNameAndTag=$reponsitoryName:$version
